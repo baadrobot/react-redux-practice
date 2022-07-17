eccb70b211a5a660771c2b741cc97e53a7b5a1f2
baadrobot <baadrobot92@gmail.com>
2022-07-17 10:38:55 +0200

    Redux practice first commit

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import logo from './logo.svg';
+import './App.css';
+import {useDispatch, useSelector} from 'react-redux';
+import { addCustomerAction, removeCustomerAction } from './store/customerReducer';
+
+function App() {
+  const dispatch = useDispatch();
+  const selectCash = useSelector(state => state.cash.cash);
+  const selectCustomers = useSelector(state => state.customers.customers)
+
+  const addCash = (selectCash) => {
+    dispatch({type: "ADD_CASH", payload: selectCash})
+  }
+  const getCash = (selectCash) => {
+    dispatch({type: "GET_CASH", payload: selectCash})
+  }
+
+  const addCustomer = (name) => {
+    const customer = {
+      name,
+      id: Date.now(),
+    }
+    dispatch(addCustomerAction(customer))
+  }
+  const removeCustomer = (customer) => {
+    dispatch(removeCustomerAction(customer.id))
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* numbers */}
+        <div style={{display: "flex"}}>
+          <button onClick={() => addCash(Number(prompt()))}>Increase</button>
+          <button onClick={() => getCash(Number(prompt()))}>Decrease</button>
+        </div>
+        <div style={{fontSize: "3rem"}}>{selectCash}</div>
+        <br/>
+        {/* customers */}
+        <div style={{display: "flex"}}>
+          <button onClick={() => addCustomer(prompt())}>Add customer</button>
+        </div>
+        {selectCustomers.length > 0
+          ?
+          <div>
+            {selectCustomers.map(customer =>
+              <div onClick={() => removeCustomer(customer)}>{customer.name}</div>
+            )}
+          </div>
+          :
+          <div>
+            No customers!
+          </div>
+        }
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Redux practice
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
